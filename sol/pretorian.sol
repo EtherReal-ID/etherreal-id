ab2fc24aca5452a403f19eed383ff10fb3ecec26
maxxflyer <maxxflyer@gmail.com>
2016-11-30 11:14:49 +0100

    considering sha3 and private id passort check

--- sol/pretorian.sol
+++ sol/pretorian.sol
@@ -7,7 +7,7 @@
 
 contract Pretorian {
 
-    etherReal_Smart_ID root;
+    Ethereal_Smart_ID root;
     address public rootOwner;
     address public rootAddress;
 
@@ -22,51 +22,54 @@ contract Pretorian {
     mapping(address => address) public registeredWalletsOwner;
 
 function Pretorian(string name,string id,string passport){
-   rootAddress=new etherReal_Smart_ID(msg.sender,name,id,passport);
+   rootAddress=new Ethereal_Smart_ID(msg.sender,name,id,passport);
    isSmartID[rootAddress]=true;
    smartIDnames[rootAddress]=name;
-   smartIDnames[rootAddress]=id;
-   smartIDid[rootAddress]=passport;
+   smartIDid[rootAddress]=sha3(id);
+   smartIDidCheck[sha3(id)]=true;
+   smartIDpassport[rootAddress]=sha3(passport);
+   smartIDpassportCheck[sha3(passport)]=true;
    rootOwner=msg.sender;
 }
 
 function registerSmartID(string name,string id,string passport) returns (bool){
 if(!isSmartID[msg.sender])throw;
-if(smartIDidCheck[id])throw;
-if(smartIDpassportCheck[passport])throw;
+if(smartIDidCheck[sha3(id)])throw;
+if(smartIDpassportCheck[sha3(passport)])throw;
 
-   address smartIDaddr=new etherReal_Smart_ID(msg.sender,name,id,passport);
+   address smartIDaddr=new Ethereal_Smart_ID(msg.sender,name,id,passport);
    isSmartID[smartIDaddr]=true;
    smartIDnames[smartIDaddr]=name;
-   smartIDnames[smartIDaddr]=id;
-   smartIDid[smartIDaddr]=passport;
+   smartIDnames[smartIDaddr]=sha3(id);
+   smartIDidCheck[sha3(id)]=true;
+   smartIDid[smartIDaddr]=sha3(passport)
+   smartIDpassportCheck[sha3(passport)]=true;
 
 }
 
 function registerWallet(address a,address owner)returns(bool){
-if(!isSmartID[msg.sender])throw;
-if(registeredWallets[a])throw;
-smartIDwallets[msg.sender].push(a);
-registeredWallets[a]=true;
-registeredWalletsOwner[a]=owner;
-
+   if(!isSmartID[msg.sender])throw;
+   if(registeredWallets[a])throw;
+   smartIDwallets[msg.sender].push(a);
+   registeredWallets[a]=true;
+   registeredWalletsOwner[a]=owner;
 }
 
 function deleteWallet(address a,address owner)returns(bool){
-if(!isSmartID[msg.sender])throw;
-if(!registeredWallets[a])throw;
-if(registeredWalletsOwner[a]!=owner)throw;
+   if(!isSmartID[msg.sender])throw;
+   if(!registeredWallets[a])throw;
+   if(registeredWalletsOwner[a]!=owner)throw;
 
 for(uint i=0;i<smartIDwallets[msg.sender].length;i++){
     if(smartIDwallets[msg.sender][i]==a)
-    smartIDwallets[msg.sender][i]=smartIDwallets[msg.sender][smartIDwallets[msg.sender].length-1];
+       smartIDwallets[msg.sender][i]=smartIDwallets[msg.sender][smartIDwallets[msg.sender].length-1];
     smartIDwallets[msg.sender][smartIDwallets[msg.sender].length-1]=0x0;
     registeredWallets[a]=false;
     }
 }
 
 function WHOIS(address a)constant returns(bool,string,string,string){
-return(isSmartID[a],smartIDnames[a],smartIDid[a],smartIDpassport[a]);
+   return(isSmartID[a],smartIDnames[a],smartIDid[a],smartIDpassport[a]);
 }
 
 
