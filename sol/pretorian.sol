996f6b05f36ae32cf5e0b3bded554d3b706f9866
maxxflyer <maxxflyer@gmail.com>
2016-12-02 20:09:52 +0100

    Update pretorian.sol

--- sol/pretorian.sol
+++ sol/pretorian.sol
@@ -11,6 +11,7 @@ contract Pretorian {
     Ethereal_Smart_ID root;
     address public rootOwner;
     address public rootAddress;
+    address public lastCreated;
 
     mapping(address => bool) public isSmartID; 
     mapping(address => string) public smartIDnames;
@@ -19,8 +20,20 @@ contract Pretorian {
     mapping(address => string) public smartIDpassport;
     mapping(string => bool) public smartIDpassportCheck;
     mapping(address => address[]) public smartIDwallets;
+    mapping(address => address) public walletSmartID;
     mapping(address => bool) public registeredWallets;
     mapping(address => address) public registeredWalletsOwner;
+    mapping(string => bool) public idRequest;
+    mapping(string => request) public idRequestsIndex;
+
+
+
+    struct request{
+    string name;
+    string id;
+    string location;
+    string owner;
+    }
 
 function Pretorian(string name,string id,string passport){
    rootAddress=new Ethereal_Smart_ID(msg.sender,name,id,passport);
@@ -33,18 +46,41 @@ function Pretorian(string name,string id,string passport){
    rootOwner=msg.sender;
 }
 
-function registerSmartID(string name,string id,string passport) returns (bool){
+function requestNewEtherRealIÐ(string name,string id,string location,bool entity)returns(bool){
+if((entity)&&(!isSmartID[msg.sender]))throw;
+if(idRequest[id])throw;
+idRequest[id]=true;
+requestblock[id]=block.number;
+request req=new request({name : name,id : id,location : location,owner : msg.sender});
+idRequestsIndex[id]=req;
+return true;
+}
+
+function resetRequest(string id)returns(bool){
+if(block.number<requestblock[id]+6000)throw;
+idRequest[id]=false;
+return true;
+}
+
+function checkRequest(string name,string id,stirng location)private constant returns (bool){
+bool temp=true;
+if(!((idRequestsIndex[id].name==name)&&(idRequestsIndex[id].id==name)&&(idRequestsIndex[id].name==location)))temp=false;
+return temp;
+}
+
+
+function registerSmartID(string name,string id,string location) returns (bool){
 if(!isSmartID[msg.sender])throw;
 if(smartIDidCheck[id])throw;
-if(smartIDpassportCheck[passport])throw;
+if(!checkRequest(name,id,location))throw;
 
-   address smartIDaddr=new Ethereal_Smart_ID(msg.sender,name,id,passport);
+   address smartIDaddr=new Ethereal_Smart_ID(idRequestsIndex[id].owner,msg.sender,name,id,passport);
    isSmartID[smartIDaddr]=true;
    smartIDnames[smartIDaddr]=name;
    smartIDnames[smartIDaddr]=id;
    smartIDidCheck[id]=true;
-   smartIDid[smartIDaddr]=passport;
-   smartIDpassportCheck[passport]=true;
+   lastCreated=smartIDaddr;
+   walletSmartID[idRequestsIndex[id].owner]=smartIDaddr;
 
 }
 
